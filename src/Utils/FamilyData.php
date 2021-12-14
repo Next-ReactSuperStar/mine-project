5d52a03df500e8ffe84d0cfb78bca5e9e1e43514
thanh-dev0306 <87410617+thanh-dev0306@users.noreply.github.com>
2021-12-14 21:34:46 +0700

    dev

---
+++ src/Utils/FamilyData.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace FamilyTree365\LaravelGedcom\Utils;
+
+use FamilyTree365\LaravelGedcom\Models\Family;
+use FamilyTree365\LaravelGedcom\Models\Person;
+
+class FamilyData
+{
+    /**
+     * Array of persons ID
+     * key - old GEDCOM ID
+     * value - new cyrus_authenticate(connection)                                   sincrement ID.
+     *
+     * @var string
+     */
+    protected $persons_id = [];
+    protected $subm_ids = [];
+    protected $sour_ids = [];
+    protected $obje_ids = [];
+    protected $note_ids = [];
+    protected $repo_ids = [];
+    protected $conn = '';
+
+    public static function getFamily($conn, $families, $obje_ids = [], $sour_ids = [], $persons_id = [], $note_ids = [], $repo_ids = [], $parentData = [])
+    {
+        $familyData = [];
+        $persons_id = [];
+
+        try {
+            foreach ($families as $family) {
+                $g_id = $family->getId();
+                $resn = $family->getResn();
+                $husb = $family->getHusb();
+                $wife = $family->getWife();
+
+                // string
+                $nchi = $family->getNchi();
+                $rin = $family->getRin();
+
+                // array
+                $subm = $family->getSubm();
+                $_slgs = $family->getSlgs();
+
+                $description = null;
+                $type_id = 0;
+
+                $children = $family->getChil();
+                $events = $family->getAllEven();
+                $_note = $family->getNote();
+                $_obje = $family->getObje();
+                $_sour = $family->getSour();
+                $_refn = $family->getRefn();
+
+                $chan = $family->getChan();
+
+                $husband_key = $parentData ? array_search($husb, array_column($parentData, 'gid')) : null;
+                $husband_uid = $parentData[$husband_key]['uid'] ?? null;
+                if (Person::where('uid', $husband_uid)->first() != null) {
+                    $husband_id = Person::where('uid', $husband_uid)->first()->id;
+                } else {
+                    $husband_id = null;
+                }
+
+                $wife_key = $parentData ? array_search($wife, array_column($parentData, 'gid')) : null;
+                $wife_uid = $parentData[$wife_key]['uid'] ?? null;
+                if (Person::where('uid', $wife_uid)->first() != null) {
+                    $wife_id = Person::where('uid', $wife_uid)->first()->id;
+                } else {
+                    $wife_id = null;
+                }
+
+                $persons_id[$husb] = $husband_id;
+                $persons_id[$wife] = $wife_id;
+
+                $value = [
+                    'husband_id'  => $husband_id,
+                    'wife_id'     => $wife_id,
+                    'description' => $description,
+                    'type_id'     => $type_id,
+                    'nchi'        => $nchi,
+                    'rin'         => $rin,
+                ];
+
+                Family::on($conn)->updateOrCreate($value, $value);
+            }
+            otherFamRecord::insertFamilyData($conn, $persons_id, $families, $obje_ids, $sour_ids);
+        } catch (\Exception $e) {
+            $error = $e->getMessage();
+
+            return \Log::error($error);
+        }
+    }
+}
---
+++ src/Utils/ParentData.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace FamilyTree365\LaravelGedcom\Utils;
+
+use Carbon\Carbon;
+use FamilyTree365\LaravelGedcom\Models\Family;
+use FamilyTree365\LaravelGedcom\Models\Person;
+use Illuminate\Support\Str;
+use Throwable;
+
+class ParentData
+{
+    /**
+     * Array of persons ID
+     * key - old GEDCOM ID
+     * value - new autoincrement ID.
+     *
+     * @var string
+     */
+    protected $persons_id = [];
+    protected $subm_ids = [];
+    protected $sour_ids = [];
+    protected $obje_ids = [];
+    protected $note_ids = [];
+    protected $repo_ids = [];
+    protected $conn = '';
+
+    public static function getPerson($conn, $individuals, $obje_ids = [], $sour_ids = [])
+    {
+        $ParentData = [];
+        $a = [];
+
+        try {
+            foreach ($individuals as $k => $individual) {
+                $g_id = $individual->getId();
+                $name = '';
+                $givn = '';
+                $surn = '';
+                $name = '';
+                $npfx = '';
+                $givn = '';
+                $nick = '';
+                $spfx = '';
+                $surn = '';
+                $nsfx = '';
+                $type = '';
+                $fone = null; // Gedcom/
+                $romn = null;
+                $names = $individual->getName();
+                $attr = $individual->getAllAttr();
+                $events = $individual->getAllEven();
+                $note = $individual->getNote();
+                $indv_sour = $individual->getSour();
+                $alia = $individual->getAlia(); // string array
+                $asso = $individual->getAsso();
+                $subm = $individual->getSubm();
+                $anci = $individual->getAnci();
+                $refn = $individual->getRefn();
+                $obje = $individual->getObje();
+                // object
+                $bapl = $individual->getBapl();
+                $conl = $individual->getConl();
+                $endl = $individual->getEndl();
+                $slgc = $individual->getSlgc();
+                $chan = $individual->getChan();
+                $g_id = $individual->getId();
+
+                if (!empty($names)) {
+                    $name = current($names)->getName() ?: '';
+                    $npfx = current($names)->getNpfx() ?: '';
+                    $givn = current($names)->getGivn() ?: '';
+                    $nick = current($names)->getNick() ?: '';
+                    $spfx = current($names)->getSpfx() ?: '';
+                    $surn = current($names)->getSurn() ?: '';
+                    $nsfx = current($names)->getNsfx() ?: '';
+                    $type = current($names)->getType() ?: '';
+                }
+
+                // array value
+                $fams = $individual->getFams();  // self family, leave it now, note would be included in family
+                $famc = $individual->getFamc();  // parent family , leave it now, note and pedi would be included in family
+
+                // added to database
+                // string value
+                $sex = preg_replace('/[^MF]/', '', $individual->getSex());
+                $uid = $individual->getUid() ?? strtoupper(str_replace('-', '', Str::uuid()));
+                $resn = $individual->getResn();
+                $rin = $individual->getRin();
+                $rfn = $individual->getRfn();
+                $afn = $individual->getAfn();
+                $titl = $individual->getAttr();
+
+                $birt = $individual->getBirt();
+                $birthday = $birt->dateFormatted ?? null;
+
+                $birth_month = $birt->month ?? null;
+                $birth_year = $birt->year ?? null;
+                $birthday_dati = $birt->dati ?? null;
+                $birthday_plac = $birt->plac ?? null;
+
+                $deat = $individual->getDeat();
+                $deathday = $deat->dateFormatted ?? null;
+                $death_month = $deat->month ?? null;
+                $death_year = $deat->year ?? null;
+                $deathday_dati = $deat->dati ?? null;
+                $deathday_plac = $deat->plac ?? null;
+                $deathday_caus = $deat->caus ?? null;
+
+                $buri = $individual->getBuri();
+                $burial_day = $buri->dateFormatted ?? null;
+                $burial_month = $buri->month ?? null;
+                $burial_year = $buri->year ?? null;
+                $burial_day_dati = $buri->dati ?? null;
+                $burial_day_plac = $buri->plac ?? null;
+
+                $chr = $individual->getChr();
+                $chr = $chr->dateFormatted ?? null;
+
+                if ($givn == '') {
+                    $givn = $name;
+                }
+
+                $config = json_encode(config('database.connections.'.$conn));
+                $value = [
+                    'gid'             => $g_id,
+                    'name'            => utf8_encode($name),
+                    'givn'            => utf8_encode($givn),
+                    'surn'            => utf8_encode($surn),
+                    'sex'             => $sex,
+                    'uid'             => $uid,
+                    'rin'             => $rin,
+                    'resn'            => $resn,
+                    'rfn'             => $rfn,
+                    'afn'             => $afn,
+                    'nick'            => $nick,
+                    'type'            => $type,
+                    'chan'            => $chan ? $chan->getDatetime() : null,
+                    'nsfx'            => $nsfx,
+                    'npfx'            => $npfx,
+                    'spfx'            => $spfx,
+                    'birthday'        => self::validateDate($birthday) ? $birthday : null,
+                    'birth_month'     => $birth_month,
+                    'birth_year'      => $birth_year,
+                    'birthday_dati'   => utf8_encode($birthday_dati),
+                    'birthday_plac'   => utf8_encode($birthday_plac),
+                    'deathday'        => $deathday,
+                    'death_month'     => $death_month,
+                    'death_year'      => $death_year,
+                    'deathday_dati'   => $deathday_dati,
+                    'deathday_plac'   => utf8_encode($deathday_plac),
+                    'deathday_caus'   => $deathday_caus,
+                    'burial_day'      => $burial_day,
+                    'burial_month'    => $burial_month,
+                    'burial_year'     => $burial_year,
+                    'burial_day_dati' => $burial_day_dati,
+                    'burial_day_plac' => $burial_day_plac,
+                    'titl'            => array_key_exists('TITL', $attr) ? $attr['TITL'][0]->getAttr('TITL') : null,
+                    'famc'            => $famc ? $famc[0]->getFamc() : null,
+                    'fams'            => $fams ? $fams[0]->getFams() : null,
+                    'chr'             => $chr,
+                ];
+
+                $parentData[] = $value;
+            }
+
+            $chunk = array_chunk($parentData, 500);
+
+            foreach ($chunk as $item) {
+                // it's take only 1 second for 3010 record
+                $a = Person::on($conn)->upsert($item, ['uid']);
+            }
+
+            otherFields::insertOtherFields($conn, $individuals, $obje_ids, $sour_ids);
+
+            return $parentData;
+        } catch (\Exception $e) {
+            return \Log::error($e);
+        }
+    }
+
+    private static function validateDate($date, $format = 'Y-m-d')
+    {
+        $d = \DateTime::createFromFormat($format, $date);
+        // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+        return $d && $d->format($format) === $date;
+    }
+}
